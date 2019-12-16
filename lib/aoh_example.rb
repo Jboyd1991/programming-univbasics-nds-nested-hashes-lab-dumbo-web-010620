fdfa76528321d7eaf172924cd39e851cdbd466c9
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-16 03:18:30 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,7 +19,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  aoh[row][key] == new_value
+  aoh[row][key] = new_value
 end
 aoh 
   # Update the AoH data at row and key to have the value of new_value
